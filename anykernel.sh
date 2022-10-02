125053f9fb9cebdbd6ea5f5f58f79ff37756a63e
nerd.zenss_ <97657256+NerdZ3ns@users.noreply.github.com>
2022-10-02 12:04:08 +0700

    anykernel: Update anykernel.sh for kernel perf

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Chronox Kernel by NerdZ3ns @ github
+kernel.string=Kernel by NerdZ3ns @ github
 do.devicecheck=1
 do.modules=0
 do.systemless=1
